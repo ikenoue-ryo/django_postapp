29773e3e37a30883dee9cfb5d0f71054cdb2348c
github ikenoue-ryo <ikenoue.ryo@gmail.com>
2020-04-16 09:52:10 +0900

    signup追加

--- postapp/forms.py
+++ postapp/forms.py
@@ -1,4 +1,16 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import get_user_model
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'profname', 'icon')
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = field.label
+
 
 class LoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
@@ -6,3 +18,5 @@ class LoginForm(AuthenticationForm):
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'
             field.widget.attrs['placeholder'] = field.label
+
+
--- postapp/views.py
+++ postapp/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .forms import LoginForm
+from .forms import LoginForm, SignUpForm
+from django.contrib.auth import login, authenticate
 
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
 from django.contrib.auth.views import LoginView, LogoutView
 
 class IndexView(TemplateView):
     template_name = 'postapp/index.html'
+
+class SignUp(CreateView):
+    form_class = SignUpForm
+    template_name = 'postapp/signup.html'
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(data=request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('postapp:index')
+        return render(request, 'postapp/signup.html', {'form': form})
+
 
 class Login(LoginView):
     form_class = LoginForm
