df6dae8798195a9aff874240ea9b74c731bf6d8f
github ikenoue-ryo <ikenoue.ryo@gmail.com>
2020-04-21 02:51:46 +0900

    詳細ページスライドショー作成

--- postapp/models.py
+++ postapp/models.py
@@ -5,6 +5,9 @@ from users.models import User
 class Post(models.Model):
     author = models.ForeignKey('users.User', on_delete=models.CASCADE)
     picture1 = models.ImageField(upload_to='image/posts/', blank=False, null=False)
+    picture2 = models.ImageField(upload_to='image/posts/', blank=True, null=True)
+    picture3 = models.ImageField(upload_to='image/posts/', blank=True, null=True)
+    picture4 = models.ImageField(upload_to='image/posts/', blank=True, null=True)
     text = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
 
