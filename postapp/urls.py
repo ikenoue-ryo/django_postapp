b4bb85858dce8588118961537eb0324564b7af51
github ikenoue-ryo <ikenoue.ryo@gmail.com>
2020-05-02 18:14:45 +0900

    未ログイン時のモーダル追加

--- postapp/urls.py
+++ postapp/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 app_name = 'postapp'
 
 urlpatterns = [
-    path('', login_required(views.IndexView.as_view()), name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('new/', login_required(views.New.as_view()), name='new'),
     path('edit/<int:pk>/', login_required(views.Edit.as_view()), name='edit'),
     path('delete/<int:pk>/', login_required(views.Delete.as_view()), name='delete'),
